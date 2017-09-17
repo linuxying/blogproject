f117cf1ba145ddcfc09f07cb9378559cc7696121
linuxying <512331228@qq.com>
2017-09-17 21:04:57 +0800

    去掉了detail模板中的评论数统计

--- blog/urls.py
+++ blog/urls.py
@@ -13,7 +13,7 @@ app_name = 'blog'
 urlpatterns = [
     # name使用方法可以看Post类下get_absolute_url方法说明
     url(r'^$', views.IndexView.as_view(), name='index'),
-    #url(r'^post/(?P<pk>[0-9]+)/$', views.detail, name='detail'),
+    # url(r'^post/(?P<pk>[0-9]+)/$', views.detail, name='detail'),
     url(r'^post/(?P<pk>[0-9]+)/$', views.PostDetailView.as_view(), name='detail'),
     # url(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$', views.archives, name='archives'),
     url(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$', views.ArchivesView.as_view(), name='archives'),
@@ -23,5 +23,5 @@ urlpatterns = [
     url(r'^admin/$', views.admin, name='admin'),
     url(r'^about/$', views.about, name='about'),
     url(r'^admin/upload/(?P<dir_name>[^/]+)$', views.upload_image, name='upload_image'),
-    url(r'^upload/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT,}),
+    url(r'^upload/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT, }),
 ]
